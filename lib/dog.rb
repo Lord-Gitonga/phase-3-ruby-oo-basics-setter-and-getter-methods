092243d3a2a7456e28c6c795b5913eee739d29d4
lordish <gitonga403@gmail.com>
2023-05-18 12:28:59 +0300

    final commit

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
 class Dog
+    def name=(name)
+        @name = name
+    end
 
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
 end
+
+fido = Dog.new
+fido.name = "Fido"
+puts fido.name
+
+breed = Dog.new
+breed.name = "Mastiff, Chihuahua, Corgi, Shar Pei, Beagie, French Bulldog, Pug, Pointer"
+puts breed.name
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
 class Person
+    def name=(name)
+        @name = name
+    end
 
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
 end
+
+my_name = Person.new
+my_name.name = "Beyonce"
+puts my_name.name
+
+my_job = Person.new
+my_job.job = "Singer"
+puts my_job.job
